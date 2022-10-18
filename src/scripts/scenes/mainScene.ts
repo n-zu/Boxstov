9cd480edd8b34ac6e27504eedf546a2ce0a9bb7c
nicolas_zulaica <nzulaica@fi.uba.ar>
2022-10-17 23:40:05 -0300

    buttons

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,27 +1,34 @@
-import PhaserLogo from '../objects/phaserLogo'
-import FpsText from '../objects/fpsText'
+import PhaserLogo from "../objects/phaserLogo";
+import FpsText from "../objects/fpsText";
+
+type ExtendedGameType = Phaser.Game & {
+  network_mode: "host" | "guest";
+};
 
 export default class MainScene extends Phaser.Scene {
-  fpsText
+  fpsText;
 
   constructor() {
-    super({ key: 'MainScene' })
+    super({ key: "MainScene" });
   }
 
   create() {
-    new PhaserLogo(this, this.cameras.main.width / 2, 0)
-    this.fpsText = new FpsText(this)
+    new PhaserLogo(this, this.cameras.main.width / 2, 0);
+    this.fpsText = new FpsText(this);
 
     // display the Phaser.VERSION
     this.add
       .text(this.cameras.main.width - 15, 15, `Phaser v${Phaser.VERSION}`, {
-        color: '#000000',
-        fontSize: '24px'
+        color: "#000000",
+        fontSize: "24px",
       })
-      .setOrigin(1, 0)
+      .setOrigin(1, 0);
+
+    const game = this.game as ExtendedGameType;
+    console.log("I am the", game.network_mode);
   }
 
   update() {
-    this.fpsText.update()
+    this.fpsText.update();
   }
 }
