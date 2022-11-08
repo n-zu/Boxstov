23fc735a84984f18d3931639c6699e48a313b886
Nicolas Zulaica <nzulaica@fi.uba.ar>
2022-11-08 18:49:05 -0300

    update enemy damage tint

--- client/src/objects/enemy.ts
+++ client/src/objects/enemy.ts
@@ -15,12 +15,11 @@ export class Enemy extends Sprite {
   id: number;
   health = HEALTH;
   facing: Direction = Direction.Down;
+  redTint = 0;
 
   constructor(scene: Phaser.Scene, x: number, y: number, id: number) {
     super(scene, x, y, "zombie");
     scene.add.existing(this);
-
-    this.scale = 0.75;
 
     this.visible = false;
     this.active = false;
@@ -43,13 +42,8 @@ export class Enemy extends Sprite {
   }
 
   public receiveDamage(damage: number) {
+    this.redTint = 1;
     if (this.health <= 0) return;
-
-    // paint red for a second
-    this.setTint(0xff0000);
-    this.scene.time.delayedCall(100, () => {
-      this.clearTint();
-    });
 
     this.health -= damage;
     if (this.health <= 0) {
@@ -64,9 +58,10 @@ export class Enemy extends Sprite {
   }
 
   private takeDamage(health: number) {
-    if (health < this.health) {
-      this.receiveDamage(this.health - health);
-    }
+    if (health < this.health) this.receiveDamage(this.health - health);
+    else this.redTint *= 0.9 / this.scene.time.timeScale;
+
+    this.setTint(0xff0000 + 0x00ffff * (1 - this.redTint));
   }
 
   private move(x: number, y: number) {
@@ -106,6 +101,8 @@ export class Enemy extends Sprite {
 
   private die() {
     this.health = 0;
+    this.setTint(0xff5555);
+    setTimeout(() => this.setTint(0xffdddd), 1000);
     this.setDepth(this.y - 100);
 
     playAnimation(
