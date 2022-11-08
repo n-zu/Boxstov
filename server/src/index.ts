3255a67d20729ef5cde66e78d38e4f72e0c81ff1
Nicolas Zulaica <nzulaica@fi.uba.ar>
2022-11-08 18:51:09 -0300

    wrap server w/ try/catch

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,24 +10,30 @@ const PORT = process.env.PORT || 9208;
 
 dotenv.config();
 
-let server;
-const app = express();
-if (process.env.PRIVATE_KEY && process.env.CERTIFICATE) {
-  const privateKey = fs.readFileSync(process.env.PRIVATE_KEY);
-  const certificate = fs.readFileSync(process.env.CERTIFICATE);
-  const credentials = { key: privateKey, cert: certificate };
-  server = https.createServer(credentials, app);
-} else {
-  server = http.createServer(app);
+console.log("Starting server...");
+
+try {
+  let server;
+  const app = express();
+  if (process.env.PRIVATE_KEY && process.env.CERTIFICATE) {
+    const privateKey = fs.readFileSync(process.env.PRIVATE_KEY);
+    const certificate = fs.readFileSync(process.env.CERTIFICATE);
+    const credentials = { key: privateKey, cert: certificate };
+    server = https.createServer(credentials, app);
+  } else {
+    server = http.createServer(app);
+  }
+
+  const game = new MultiplayerGame(server, new HostMaster(server));
+  app.use("/", express.static("./dist"));
+
+  app.get("/health", (req: Request, res: Response) => {
+    res.send("OK");
+  });
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+} catch (err) {
+  console.log(err);
 }
-
-const game = new MultiplayerGame(server, new HostMaster(server));
-app.use("/", express.static("./dist"));
-
-app.get("/health", (req: Request, res: Response) => {
-  res.send("OK");
-});
-
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
