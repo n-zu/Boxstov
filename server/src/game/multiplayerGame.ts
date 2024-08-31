6558b0a1e8bb97dba74abd1891a83b06e2d8d218
efoppiano <efoppiano@fi.uba.ar>
2024-08-30 22:14:57 -0300

    Server: Remove circular dependencies between World and MainScene

--- server/src/game/multiplayerGame.ts
+++ server/src/game/multiplayerGame.ts
@@ -3,19 +3,28 @@ import Phaser from "phaser";
 import { GameMaster } from "../gameMaster/gameMaster.js";
 import { config } from "../config.js";
 import MainScene from "../scenes/mainScene.js";
+import GameObserver from "../../../common/observer/gameObserver.js";
+import { GameEvents } from "../types/events.js";
+import Observer from "../../../common/observer/observer.js";
+import { World } from "../objects/world.js";
 
 export class MultiplayerGame extends Phaser.Game {
   mainScene: MainScene;
+  observer: Observer<GameEvents>;
+  world: World;
 
   constructor(gameMaster: GameMaster, onEnd: () => void) {
     super(config);
-    this.mainScene = this.scene.add("MainScene", MainScene, true, {
-      gameMaster,
-      onEnd,
-    }) as MainScene;
+    let observer = new GameObserver();
+    this.observer = observer;
+    function createScene() {
+      return new MainScene(observer);
+    }
+    this.mainScene = this.scene.add("MainScene", createScene, true) as MainScene;
+    this.world = new World(this.mainScene, this.observer, gameMaster, onEnd);
   }
 
   public addPlayer(id: string): boolean {
-    return this.mainScene.addPlayer(id) || false;
+    return this.world.addPlayer(id) || false;
   }
 }
--- server/src/scenes/mainScene.ts
+++ server/src/scenes/mainScene.ts
@@ -1,43 +1,20 @@
-import { World } from "../objects/world.js";
-import { GameMaster } from "../gameMaster/gameMaster.js";
-import { MS_BETWEEN_SYNCS, WORLD_BOUNDS } from "../../../common/constants.js";
-import GameObserver from "../../../common/observer/gameObserver.js";
+import { WORLD_BOUNDS } from "../../../common/constants.js";
 import Observer from "../../../common/observer/observer.js";
 import { GameEvents } from "../types/events.js";
 
-type MainSceneData = {
-  gameMaster: GameMaster;
-  onEnd: () => void;
-};
-
 export default class MainScene extends Phaser.Scene {
-  world!: World;
-  gameMaster?: GameMaster;
-  onEnd?: () => void;
   observer: Observer<GameEvents>;
-  lastSyncTimestamp = 0;
 
-  protected constructor() {
+  constructor(observer: Observer<GameEvents>) {
     super({ key: "MainScene" });
-    this.observer = new GameObserver();
+    this.observer = observer;
   }
 
-  create({ gameMaster, onEnd }: MainSceneData) {
-    this.gameMaster = gameMaster;
-    this.world = new World(this, this.observer, gameMaster, onEnd);
-    this.world.create();
+  create() {
     this.physics.world.setBounds(...WORLD_BOUNDS)
   }
 
   update() {
-    this.world.update();
-    if (Date.now() - this.lastSyncTimestamp > MS_BETWEEN_SYNCS) {
-      this.gameMaster?.broadcast("sync", this.world.getState());
-      this.lastSyncTimestamp = Date.now();
-    }
-  }
-
-  public addPlayer(id: string): boolean {
-    return this.world?.addPlayer(id);
+    this.observer.notify("tick");
   }
 }
