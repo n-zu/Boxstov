bfc75a206eda80bbcecfe24e42825ea2e6ed4deb
efoppiano <efoppiano@fi.uba.ar>
2024-08-30 23:09:13 -0300

    Remove onEnd callback and make it an event

--- server/src/game/multiplayerGame.ts
+++ server/src/game/multiplayerGame.ts
@@ -3,7 +3,6 @@ import Phaser from "phaser";
 import { GameMaster } from "../gameMaster/gameMaster.js";
 import { config } from "../config.js";
 import MainScene from "../scenes/mainScene.js";
-import GameObserver from "../../../common/observer/gameObserver.js";
 import { GameEvents } from "../types/events.js";
 import Observer from "../../../common/observer/observer.js";
 import { World } from "../objects/world.js";
@@ -18,18 +17,17 @@ export class MultiplayerGame extends Phaser.Game {
   gameMaster: GameMaster;
   lastSyncTimestamp = 0;
 
-  constructor(gameMaster: GameMaster, onEnd: () => void) {
+  constructor(gameMaster: GameMaster, observer: Observer<GameEvents>) {
     super(config);
     this.gameMaster = gameMaster;
     this.setupGameMaster(gameMaster);
 
-    let observer = new GameObserver();
     this.observer = observer;
     function createScene() {
       return new MainScene(observer);
     }
     this.mainScene = this.scene.add("MainScene", createScene, true) as MainScene;
-    this.world = new World(this.mainScene, this.observer, onEnd);
+    this.world = new World(this.mainScene, this.observer);
 
     this.observer.subscribe("tick", () => this.sync());
   }
