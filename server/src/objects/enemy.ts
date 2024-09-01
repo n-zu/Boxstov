d6302a22b21a77758d17ef66938753ed88152405
efoppiano <efoppiano@fi.uba.ar>
2024-09-01 02:53:16 -0300

    Move enemy movement logic to EnemyBrain

--- server/src/objects/enemy.ts
+++ server/src/objects/enemy.ts
@@ -7,19 +7,21 @@ import Observer from "../../../common/observer/observer.js";
 import EnemyBrain from "./enemyBrain.js";
 import { GameEvents } from "../types/events.js";
 import { ZOMBIE_SIZE, ZOMBIE_SPEED } from "../../../common/constants.js";
-
-const HEALTH = 100;
+import { UnitVector } from "../../../common/types/direction.js";
 
 export class Enemy extends Phaser.Physics.Arcade.Sprite {
   id: number;
-  health = HEALTH;
-  strength = 3;
+  maxHealth: number;
+  health: number;
+  strength: number;
+  speed: number;
+  attackRange: number;
+
   movementDirection: MovementDirection = new MovementDirection();
   observer: Observer<GameEvents>;
   brain: EnemyBrain;
   action = "walk";
   dead = true;
-  speed: number;
   events: EnemyRecentEvents[] = [];
 
   constructor(
@@ -27,46 +29,42 @@ export class Enemy extends Phaser.Physics.Arcade.Sprite {
     x: number,
     y: number,
     id: number,
-    observer: Observer<GameEvents>
+    observer: Observer<GameEvents>,
+    health: number = 100,
+    strength: number = 3,
+    attackRange: number = 150,
   ) {
     super(scene, x, y, "zombie");
-    scene.physics.add.existing(this);
-    scene.add.existing(this);
+    this.addToScene();
+
+    this.id = id;
+    this.observer = observer;
+    const isFast = Math.random() > 0.8;
+    this.speed = isFast ? ZOMBIE_SPEED.FAST : ZOMBIE_SPEED.SLOW;
+    this.brain = new EnemyBrain(Math.random() * 100);
+
+    this.maxHealth = health;
+    this.health = health;
+    this.strength = strength;
+    this.attackRange = attackRange;
+  }
+
+  private addToScene() {
+    this.scene.add.existing(this);
+    this.scene.physics.add.existing(this);
     this.setBodySize(...ZOMBIE_SIZE);
 
     this.visible = false;
     this.active = false;
-    const isFast = Math.random() > 0.8;
-    this.speed = isFast ? ZOMBIE_SPEED.FAST : ZOMBIE_SPEED.SLOW;
-
-    this.brain = new EnemyBrain();
-    this.id = id;
-    this.observer = observer;
   }
 
   public async update(players: Player[]) {
-    if (!this.body.enable) return;
+    this.brain.update(this, players);
+  }
 
-    const canThink = this.brain.think();
-    if (!canThink) return;
-    if (players.length === 0) return;
-
-    const closestPlayer = this.getClosestPlayer(players);
-    const dx = closestPlayer.x - this.x;
-    const dy = closestPlayer.y - this.y;
-
-    const distance = Math.sqrt(dx * dx + dy * dy);
-    const isFar = distance > 150 ? 1 : 0;
-
-    this.movementDirection.update([dx / distance, dy / distance]);
-    this.setVelocity(...this.movementDirection.getSpeed(this.speed * isFar));
-
-    this.action = isFar ? "walk" : "atk";
-    if (!isFar) {
-      closestPlayer.receiveDamage(
-        (this.strength * this.scene.game.loop.delta) / 100
-      );
-    }
+  public turn(v: UnitVector) {
+    this.movementDirection.update(v);
+    this.setVelocity(...this.movementDirection.getSpeed(this.speed));
   }
 
   public getState(): EnemyState {
@@ -101,37 +99,11 @@ export class Enemy extends Phaser.Physics.Arcade.Sprite {
 
   public spawn(x: number, y: number) {
     this.setPosition(x, y);
-    this.health = HEALTH;
+    this.health = this.maxHealth;
     this.setActive(true);
     this.setVisible(true);
     this.body.enable = true;
     this.dead = false;
-  }
-
-  // Rounds the angle to one of 8 directions
-  private roundAngle(angle: number) {
-    return (Math.round((angle * 4) / Math.PI) * Math.PI) / 4;
-  }
-
-  private getClosestPlayer(players: Player[]): Player {
-    let closestPlayer: Player = players[0];
-    let distanceToClosestPlayer: number | null = null;
-    for (const player of players) {
-      const distance = Phaser.Math.Distance.Between(
-        this.x,
-        this.y,
-        player.x,
-        player.y
-      );
-      if (
-        distanceToClosestPlayer === null ||
-        distance < distanceToClosestPlayer
-      ) {
-        closestPlayer = player;
-        distanceToClosestPlayer = distance;
-      }
-    }
-    return closestPlayer;
   }
 
   private beKilledBy(killerId: string) {
--- server/src/objects/enemyBrain.ts
+++ server/src/objects/enemyBrain.ts
@@ -1,18 +1,65 @@
+import { Player } from "../player/player";
+import { Enemy } from "./enemy";
+
 export default class EnemyBrain {
   cooldown: number;
   cooldownCount: number;
 
-  constructor(cooldown: number = Math.random() * 100) {
+  constructor(cooldown: number) {
     this.cooldown = cooldown;
     this.cooldownCount = cooldown;
   }
 
-  public think(): boolean {
+  private canThink(): boolean {
     this.cooldownCount--;
     if (this.cooldownCount <= 0) {
       this.cooldownCount = this.cooldown;
       return true;
     }
     return false;
+  }
+
+  public update(me: Enemy, players: Player[]) {
+    if (!me.body.enable) return;
+
+    if (!this.canThink()) return;
+    if (players.length === 0) return;
+
+    const closestPlayer = this.getClosestPlayer(me, players);
+    const dx = closestPlayer.x - me.x;
+    const dy = closestPlayer.y - me.y;
+
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    const isFar = distance > me.attackRange;
+
+    me.turn([dx / distance, dy / distance]);
+
+    me.action = isFar ? "walk" : "atk";
+    if (!isFar) {
+      closestPlayer.receiveDamage(
+        (me.strength * me.scene.game.loop.delta) / 100
+      );
+    }
+  }
+
+  private getClosestPlayer(me: Enemy, players: Player[]): Player {
+    let closestPlayer: Player = players[0];
+    let distanceToClosestPlayer: number | null = null;
+    for (const player of players) {
+      const distance = Phaser.Math.Distance.Between(
+        me.x,
+        me.y,
+        player.x,
+        player.y
+      );
+      if (
+        distanceToClosestPlayer === null ||
+        distance < distanceToClosestPlayer
+      ) {
+        closestPlayer = player;
+        distanceToClosestPlayer = distance;
+      }
+    }
+    return closestPlayer;
   }
 }
