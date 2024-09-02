debde9c1f607633c9d4e3f0f58ee6f3906ef0326
efoppiano <efoppiano@fi.uba.ar>
2024-09-01 23:14:49 -0300

    Fix run.sh script

--- server/scripts/run.sh
+++ server/scripts/run.sh
@@ -1,38 +1,31 @@
 #!/bin/bash
 
-# cd server
-# npm ci
-# npm run build
-tmux kill-session -t boxstov_server 2>/dev/null
-tmux new -s boxstov_server -d 'npm run start_cheap'
+session_name=boxstov_server
+command="npm run start_cheap"
+expected_message="Listening on port"
 
-while true; do
+cd /home/boxstov/server
+npm ci
+npm run build
 
-  # Capture the output of the tmux session
-  result=$(tmux capture-pane -pt boxstov_server)
+tmux has-session -t $session_name 2>/dev/null
+if [ $? == 0 ]; then
+    tmux kill-session -t $session_name
+fi
+tmux new -s $session_name -d $command
 
-  # Check if tmux session output retrieval failed
-  if [ $? -ne 0 ]; then
-    echo "Server failed to start!"
-    exit 1
-  fi
+while true
+do
+    tmux has-session -t $session_name 2>/dev/null
+    if [ $? != 0 ]; then
+        echo "Error while deploying"
+        exit 1
+    fi
+    if tmux capture-pane -J -pt $session_name | grep -q "$expected_message"; then
+        break
+    fi
 
-  # Check if the output contains 'Error'
-  echo "$result" | grep -q 'Error'
-  if [ $? -eq 0 ]; then
-    echo "Server failed to start!"
-    exit 1
-  fi
-
-  # Check if the output contains the success message
-  echo "$result" | grep -q 'action: server_up | result: success'
-  if [ $? -eq 0 ]; then
-    echo "Server started successfully!"
-    break
-  fi
-
-  # Wait a bit before checking again
-  sleep 1
+    sleep 0.5
 done
 
 echo "Deployment complete."
