67b72a7772c34c9317a9b78a854b2dde64b1d884
Nicolas Zulaica <66538092+n-zu@users.noreply.github.com>
2024-09-22 00:41:52 -0300

    chore: use https

--- client/webpack/webpack.prod.cjs
+++ client/webpack/webpack.prod.cjs
@@ -45,7 +45,7 @@ const prod = {
       ],
     }),
     new DefinePlugin({
-      "process.env.SERVER_URL": JSON.stringify("http://rostov-vps-1.duckdns.org"),
+      "process.env.SERVER_URL": JSON.stringify("https://rostov-vps-1.duckdns.org"),
     }),
   ],
 };
